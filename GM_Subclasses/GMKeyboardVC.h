aae94316bdbd1ea3e8ce61521b3e5b9e35d32c2e
Gary A. Morris <gary@mggm.net>
2011-03-28 08:09:19 -0700

    New subclass of UIViewController that handles slide up keyboard by automatically moving the view so that the field being edited is visible above the keyboard.

---
+++ GM_Subclasses/GMKeyboardVC.h
@@ -0,0 +1,26 @@
+//
+//  GMKeyboardViewConroller.h
+//  NextSprinter
+//
+//  Created by Gary Morris on 3/26/11.
+//  Copyright 2011 Gary A. Morris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// This UIViewController subclass handles keyboard notifications and 
+// moves the view so that the field being edited is not obsucred by
+// the popup keyboard
+
+@interface GMKeyboardVC : UIViewController {
+    
+    CGRect beforeFrame;             // frame before keyboard appeared
+    
+    NSTimeInterval animDuration;    // keyboard animation duration  
+    UIViewAnimationCurve animCurve; // keyboard animation curve
+    
+    BOOL viewMovedUp;
+    
+}
+
+@end
---
+++ GM_Subclasses/GMKeyboardVC.m
@@ -0,0 +1,114 @@
+//
+//  GMKeyboardViewConroller.m
+//  NextSprinter
+//
+//  Created by Gary Morris on 3/26/11.
+//  Copyright 2011 Gary A. Morris. All rights reserved.
+//
+
+#import "GMKeyboardVC.h"
+#import "Utilities.h"
+
+@implementation GMKeyboardVC
+
+//---------------------------------------------------------------------------
+// keyboardWillShow - moves view controller's view up so that the field
+//    that is firstResponder will be visible above the keyboard
+//---------------------------------------------------------------------------
+-(void)keyboardWillShow:(NSNotification*)aNotification
+{
+    // Save the view's frame before the keyboard appeared
+	beforeFrame = self.view.frame;
+	
+    // Get the size of the keyboard.
+    NSDictionary* info  = [aNotification userInfo];
+    NSValue* aValue     = [info objectForKey:UIKeyboardBoundsUserInfoKey];
+    CGSize keyboardSize = [aValue CGRectValue].size;
+
+    // Get the animation duration
+    NSNumber* aNumber = [info objectForKey:UIKeyboardAnimationDurationUserInfoKey];
+    animDuration = aNumber.doubleValue;
+    
+    // Get the animation curve
+    aNumber = [info objectForKey:UIKeyboardAnimationCurveUserInfoKey];
+    animCurve = aNumber.intValue;
+    
+    // Find the first responder that caused the keyboard to appear
+    CGRect firstRespFrame = self.view.bounds;         // in case not found
+    for (UIView* v in self.view.subviews) {
+        if ([v isFirstResponder]) {
+            firstRespFrame = v.frame;
+            break;
+        }
+    }
+    CGFloat curBottom = firstRespFrame.origin.y + firstRespFrame.size.height;
+    CGFloat newBottom = self.view.frame.size.height - keyboardSize.height - 20;
+    CGFloat deltaY = curBottom - newBottom;
+    
+    // Move the view controller's view up because of the keyboard
+    if (deltaY > 0) {
+        CGRect newFrame = self.view.frame;
+        newFrame.origin.y -= deltaY;
+        
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:animDuration];
+        [UIView setAnimationCurve:animCurve];
+        
+        self.view.frame = newFrame;
+        viewMovedUp = YES;
+        
+        [UIView commitAnimations];
+    }
+}
+
+//---------------------------------------------------------------------------
+// keyboardWillHide - put view back where it belongs
+//---------------------------------------------------------------------------
+-(void)keyboardWillHide:(NSNotification*)aNotification
+{
+    if (viewMovedUp) {
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:animDuration];
+        [UIView setAnimationCurve:animCurve];
+        
+        // Set the view controller's view back to where it was
+        self.view.frame = beforeFrame;
+        viewMovedUp = NO;
+        
+        [UIView commitAnimations];
+    }
+}
+
+//---------------------------------------------------------------------------
+// viewWillAppear - subscribe to keyboard notifications
+//---------------------------------------------------------------------------
+-(void)viewWillAppear:(BOOL)animated
+{
+    viewMovedUp = NO;
+    [[NSNotificationCenter defaultCenter] addObserver: self 
+                                             selector: @selector(keyboardWillShow:)
+                                                 name: UIKeyboardWillShowNotification 
+                                               object: nil];
+    [[NSNotificationCenter defaultCenter] addObserver: self 
+                                             selector: @selector(keyboardWillHide:)
+                                                 name: UIKeyboardWillHideNotification 
+                                               object: nil];
+    [super viewWillAppear:animated];
+}
+
+//---------------------------------------------------------------------------
+// viewWillDisappear - unsubscribe to keyboard notifications
+//---------------------------------------------------------------------------
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];	
+    
+    if (viewMovedUp) {
+        self.view.frame = beforeFrame;      // undo the move
+        viewMovedUp = NO;
+    }
+    
+    [super viewWillDisappear:animated];
+}
+
+@end
