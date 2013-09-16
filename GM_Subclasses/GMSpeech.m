6cbcf29273d09804847bc7f46e146ff8040f61cd
Gary A. Morris <gmorris@esri.com>
2013-09-16 10:01:19 -0700

    updated copyright info with GNU Lesser General Public License

--- GM_Subclasses/GMSpeech.m
+++ GM_Subclasses/GMSpeech.m
@@ -7,6 +7,19 @@
 //
 //  Modified to work with both OSX and iOS 7.0+
 //
+// This is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This file is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this file. If not, see <http://www.gnu.org/licenses/>.
+//
 #import "GMSpeech.h"
 
 // speech synthesis requires MacOSX or iOS >= 7
