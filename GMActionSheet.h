81c7a988a27656a4db2c5edcdde38d0819029e33
Gary A. Morris <gmorris@esri.com>
2013-09-16 09:58:12 -0700

    updated copyright info with GNU Lesser General Public License

--- GMActionSheet.h
+++ GMActionSheet.h
@@ -5,6 +5,21 @@
 //  Created by Gary Morris on 11/21/11.
 //  Copyright (c) 2011 Gary A. Morris. All rights reserved.
 //
+// This file is part of SDK_Utilities.repo
+//
+// This is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This file is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this file. If not, see <http://www.gnu.org/licenses/>.
+//
 
 #import <UIKit/UIKit.h>
 
--- GMAlertView.m
+++ GMAlertView.m
@@ -4,6 +4,21 @@
 //  Created by Gary Morris on 8/19/11.
 //  Code originally from http://stackoverflow.com/questions/3105974/dismissing-uialertviews-when-entering-background-state/3181223#3181223
 //
+// This file is part of SDK_Utilities.repo
+//
+// This is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This file is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this file. If not, see <http://www.gnu.org/licenses/>.
+//
 
 #import "GMAlertView.h"
 #import "Utilities.h"
